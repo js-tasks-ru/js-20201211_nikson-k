771b942d24d8feb87dda009344f13da194e7d33c
KilovatiyNikita <nikson.k@gmail.com>
2020-12-11 21:31:38 +0300

    implement sum function

--- 01-intro/1-sum/index.js
+++ 01-intro/1-sum/index.js
@@ -5,5 +5,10 @@
  * @returns {number}
  */
 export default function sum(m, n) {
-
+  if (typeof m != 'number' || typeof n != 'number') {
+    throw new Error('Input parameters should not be empty');
+  }
+  else {
+    return m + n;
+  }
 }
