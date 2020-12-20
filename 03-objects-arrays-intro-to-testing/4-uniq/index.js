b1dd389156655e07589b53eb5e68bb7248689afc
KilovatiyNikita <nikson.k@gmail.com>
2020-12-20 19:28:17 +0300

    implement uniq function

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,16 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  if (!arr) {
+    return [];
+  }
 
+  let map = new Map();
+  arr.forEach(e => {
+    if (!map.has(e)) {
+      map.set(e, 1);
+    }
+  });
+
+  return Array.from(map.keys());
 }
