1aaaa295bb7f14533ddb37cd8226a3bec67113d5
KilovatiyNikita <nikson.k@gmail.com>
2020-12-20 19:11:40 +0300

    implement trimSymbols function

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,29 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) {
+    return '';
+  }
 
+  if (!size){
+    return string;
+  }
+
+  const collector = [];
+  let counter = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] !== collector[collector.length - 1]) {
+      counter = 1;
+      collector.push(string[i]);
+      continue;
+    }
+    if (string[i] === collector[collector.length - 1] && counter < size) {
+      collector.push(string[i]);
+      counter++;
+      continue;
+    }
+  }
+
+  return collector.join('');
 }
