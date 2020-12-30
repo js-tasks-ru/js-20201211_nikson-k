ee49ab67a690f1fbf6793bed9a4d97be58b1a62d
KilovatiyNikita <nikson.k@gmail.com>
2020-12-30 23:35:41 +0300

    implement Tooltip component

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,5 +1,45 @@
 class Tooltip {
+  constructor() {
+    this.onMouseMove = (event) => {
+      const tooltipText = event.target.dataset.tooltip;
+      if (tooltipText) {
+        this.remove();
+        this.render(tooltipText);
+        this.element.style.top = (event.clientY + 20) + 'px';
+        this.element.style.left = (event.clientX + 20) + 'px';
+      }
+    };
+    this.onPointerOut = (event) => {
+      this.element.remove();
+    }
+  }
+  initialize() {
+    document.body.addEventListener('mousemove', this.onMouseMove);
+    document.body.addEventListener('pointerover', this.onMouseMove);
+    document.body.addEventListener('pointerout', this.onPointerOut);
+  }
 
+  render(tooltipText) {
+    const tmpContainer = document.createElement('div');
+    tmpContainer.innerHTML = this.getTemplate(tooltipText);
+    this.element = tmpContainer.firstChild;
+    document.body.append(this.element);
+  }
+
+  getTemplate(text) {
+    return `<div class="tooltip">${text}</div>`;
+  }
+  destroy() {
+    this.remove();
+    document.body.removeEventListener('mousemove', this.onMouseMove);
+    document.body.removeEventListener('pointerover', this.onMouseMove);
+    document.body.removeEventListener('pointerout', this.onPointerOut);
+  }
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
 }
 
 const tooltip = new Tooltip();
